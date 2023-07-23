bb7506c70bce750b1c49cbf1c2a460395c16d6bc
KB <79893099+KB-112@users.noreply.github.com>
2023-07-24 04:16:54 +0530

    New Features intro in spinner

--- Square/Assets/1/Script/Menu/MenuToggle.cs
+++ Square/Assets/1/Script/Menu/MenuToggle.cs
@@ -2,50 +2,101 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class MenuToggle : MonoBehaviour
 {
-    public GameObject WatchedAds;
-    public GameObject TimeskipAds;
-    public GameObject WatchAdsBg;
+    [SerializeField] private GameObject WatchedAds;
+    [SerializeField] private GameObject TimeskipAds;
+    [SerializeField] private GameObject WatchAdsBg;
 
 
-    public Button WatchAdsBtn;
-    public Button CloseAdsBtn;
-    public Button TimeSkipAdsBtn;
-    public Button OkayBtn; 
+    [SerializeField] private GameObject WheelSpinnerBg;
+    [SerializeField] private GameObject TapToskip;
+
+
+    // Cache the button references
+    [SerializeField] private Button WatchAdsBtn;
+    [SerializeField] private Button CloseAdsBtn;
+    [SerializeField] private Button TimeSkipAdsBtn;
+    [SerializeField] private Button OkayBtn;
+
+    [SerializeField] private Button SpinWheelerBtn;
+    [SerializeField] private Button CloseWheelerBtn;
+    [SerializeField] private Button SpinNowBtn;
+
+
+
+    [SerializeField] public float wheelerTImeClosed;
+
+    bool startWheelerTimer;
+
+
+    private void Awake()
+    {
+       TapToskip.SetActive(false);
+    }
     private void Start()
     {
-       
-
-        WatchAdsBtn.onClick.AddListener(WantToSeeAdsWindow);
-       TimeSkipAdsBtn.onClick.AddListener(TimeSkipWindow);
-        CloseAdsBtn.onClick.AddListener(QuitWatchAdsWindow);
-        OkayBtn.onClick.AddListener(QuitTimeskipWindow);
-
+        // Add listeners to buttons
+        WatchAdsBtn.onClick.AddListener(ShowWatchAdsWindow);
+        TimeSkipAdsBtn.onClick.AddListener(ShowTimeSkipWindow);
+        CloseAdsBtn.onClick.AddListener(CloseWatchAdsWindow);
+        OkayBtn.onClick.AddListener(CloseTimeSkipWindow);
+        SpinWheelerBtn.onClick.AddListener(SpinWheelerActivation);
+        CloseWheelerBtn.onClick.AddListener(CloseWheeler);
+        SpinNowBtn.onClick.AddListener(SpinHere);
 
     }
-    public void WantToSeeAdsWindow()
+
+    public void ShowWatchAdsWindow()
     {
         WatchedAds.SetActive(true);
         WatchAdsBg.SetActive(true);
     }
-    public void TimeSkipWindow()
+
+    public void ShowTimeSkipWindow()
     {
         WatchedAds.SetActive(false);
         TimeskipAds.SetActive(true);
     }
-     
 
-    public void QuitWatchAdsWindow()
+    public void CloseWatchAdsWindow()
     {
         WatchedAds.SetActive(false);
         WatchAdsBg.SetActive(false);
     }
 
-    public void QuitTimeskipWindow()
+    public void CloseTimeSkipWindow()
     {
         TimeskipAds.SetActive(false);
         WatchAdsBg.SetActive(false);
     }
 
+    public void SpinWheelerActivation()
+    {
+
+        WheelSpinnerBg.SetActive(true);
+        startWheelerTimer = false;
+    }
+
+    public void CloseWheeler()
+    {
+       WheelSpinnerBg.SetActive(false);
+        TapToskip.SetActive(false);
+    }
+    IEnumerator ExitWheeler()
+    {
+        yield return new WaitForSeconds(wheelerTImeClosed);
+        TapToskip.SetActive(true);
+    }
+
+    public void SpinHere()
+    {
+        startWheelerTimer = true;
+        if (startWheelerTimer)
+        {
+            StartCoroutine(ExitWheeler());
+           
+        }
+    }
 }
